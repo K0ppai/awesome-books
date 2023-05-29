f2f804c3e9d07f1ba8e357e1ea232ca79ff69f7e
rmiyoyo <miyoyoraphael@gmail.com>
2023-05-29 18:50:11 +0300

    Fix linter errors in app.js file

--- app.js
+++ app.js
@@ -7,60 +7,18 @@ const bookData = JSON.parse(localStorage.getItem('bookData')) || [];
 // Select the book list container element
 const ul = document.getElementById('book-list-container');
 
-// Event listener for form submission
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  // Get the input values for title and author
-  const title = form.querySelector('input[placeholder="Title"]').value;
-  const author = form.querySelector('input[placeholder="Author"]').value;
-
-  // Create a new data object
-  const newData = {
-    Title: title,
-    Author: author
-  };
-
-  // Add the new data to the bookData array
-  bookData.push(newData);
-
-  // Save the updated bookData to local storage
-  saveDataToLocalStorage();
-
-  // Render the updated book list
-  renderBookList();
-
-  // Reset the form input fields
-  form.reset();
-});
-
-// Event listener for removing a book
-ul.addEventListener('click', (e) => {
-  if (e.target.tagName === 'BUTTON') {
-    // Get the parent li element of the clicked button
-    const li = e.target.parentElement;
-
-    // Find the index of the li element within the book list container
-    const index = Array.from(ul.children).indexOf(li);
-
-    // Remove the corresponding data object from the bookData array
-    bookData.splice(index, 1);
-
-    // Save the updated bookData to local storage
-    saveDataToLocalStorage();
-
-    // Render the updated book list
-    renderBookList();
-  }
-});
+// Function to save the bookData to local storage
+function saveDataToLocalStorage() {
+  localStorage.setItem('bookData', JSON.stringify(bookData));
+}
 
 // Function to render the book list on the page
 function renderBookList() {
   // Clear the existing book list
   ul.innerHTML = '';
 
-  // Iterate over the bookData array and create list items for each book
-  for (const data of bookData) {
+  // Iterate over the bookData array using forEach method
+  bookData.forEach((data) => {
     // Create li element
     const li = document.createElement('li');
 
@@ -86,13 +44,55 @@ function renderBookList() {
 
     // Append the li element to the book list container
     ul.appendChild(li);
-  }
+  });
 }
 
-// Function to save the bookData to local storage
-function saveDataToLocalStorage() {
-  localStorage.setItem('bookData', JSON.stringify(bookData));
-}
+// Event listener for form submission
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get the input values for title and author
+  const title = form.querySelector('input[placeholder="Title"]').value;
+  const author = form.querySelector('input[placeholder="Author"]').value;
+
+  // Create a new data object
+  const newData = {
+    Title: title,
+    Author: author,
+  };
+
+  // Add the new data to the bookData array
+  bookData.push(newData);
+
+  // Save the updated bookData to local storage
+  saveDataToLocalStorage();
+
+  // Render the updated book list
+  renderBookList();
+
+  // Reset the form input fields
+  form.reset();
+});
+
+// Event listener for removing a book
+ul.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    // Get the parent li element of the clicked button
+    const li = e.target.parentElement;
+
+    // Find the index of the li element within the book list container
+    const index = Array.prototype.indexOf.call(ul.children, li);
+
+    // Remove the corresponding data object from the bookData array
+    bookData.splice(index, 1);
+
+    // Save the updated bookData to local storage
+    saveDataToLocalStorage();
+
+    // Render the updated book list
+    renderBookList();
+  }
+});
 
 // Render the initial book list
 renderBookList();
