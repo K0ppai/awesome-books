52b867ccc4b79a586a528c1196afdd5f63759677
raphaelmiy <raphaelmiyoyo14@gmail.com>
2023-05-30 19:04:46 +0300

    Revert "Implement the functionalities using JavaScript classes instead of objects"
    This reverts commit 49837df358f43e2b70837ba7659388cc084a067e.

--- app.js
+++ app.js
@@ -1,86 +1,77 @@
-class BookListApp {
-  constructor() {
-    this.form = document.querySelector('.form');
-    this.bookData = JSON.parse(localStorage.getItem('bookData')) || [];
-    this.ul = document.getElementById('book-list-container');
+const form = document.querySelector('.form');
+const bookData = JSON.parse(localStorage.getItem('bookData')) || [];
+const ul = document.getElementById('book-list-container');
 
-    this.form.addEventListener('submit', this.handleFormSubmit.bind(this));
-    this.ul.addEventListener('click', this.handleRemoveButtonClick.bind(this));
-
-    this.renderBookList();
-  }
-
-  saveDataToLocalStorage() {
-    localStorage.setItem('bookData', JSON.stringify(this.bookData))
-  }
-
-  renderBookList() {
-    this.ul.innerHTML = '';
-
-    this.bookData.forEach((book, index) => {
-      const li = document.createElement('li');
-      li.className = 'px-3 py-2 d-flex fw-bold fs-5';
-
-      const titleSpan = document.createElement('span');
-      titleSpan.className = 'title';
-      titleSpan.innerText = `"${book.Title}"`;
-
-      const authorSpan = document.createElement('span');
-      authorSpan.className = 'author';
-      authorSpan.innerText = book.Author;
-
-      const byText = document.createElement('span');
-      byText.innerText = 'by';
-      byText.className = 'ps-3 pe-3';
-
-      const removeBtn = document.createElement('button');
-      removeBtn.className = 'remove-btn fw-bold btn btn-outline-dark btn-sm ms-auto';
-      removeBtn.innerText = 'Remove';
-
-      if (index % 2 !== 0) {
-        li.className += ' bg-warning';
-      }
-      li.append(titleSpan, byText, authorSpan, removeBtn);
-      this.ul.appendChild(li);
-    });
-  }
-
-  handleFormSubmit(e) {
-    e.preventDefault();
-
-    const title = this.form.querySelector('input[placeholder="Title"]').value;
-    const author = this.form.querySelector('input[placeholder="Author"]').value;
-
-    const newBook = {
-      Title: title,
-      Author: author,
-    };
-
-    this.addBook(newBook);
-    this.renderBookList();
-    this.form.reset();
-  }
-
-  handleRemoveButtonClick(e) {
-    if (e.target.tagName === 'BUTTON') {
-      const li = e.target.parentElement;
-      const index = Array.prototype.indexOf.call(this.ul.children, li);
-
-      this.removeBook(index);
-      this.renderBookList();
-    }
-  }
-
-  addBook(book) {
-    this.bookData.push(book);
-    this.saveDataToLocalStorage();
-  }
-
-  removeBook(index) {
-    this.bookData.splice(index, 1);
-    this.saveDataToLocalStorage();
-  }
-
+function saveDataToLocalStorage() {
+  localStorage.setItem('bookData', JSON.stringify(bookData));
 }
 
-const app = new BookListApp();
+function renderBookList() {
+  ul.innerHTML = '';
+  // bookData.forEach((data) => {
+  for (let i = 0; i < bookData.length; i += 1) {
+    const li = document.createElement('li');
+    li.className = 'px-3 py-2 d-flex fw-bold fs-5';
+
+    const titleSpan = document.createElement('span');
+    titleSpan.className = 'title';
+    titleSpan.innerText = `"${bookData[i].Title}"`;
+
+    const authorSpan = document.createElement('span');
+    authorSpan.className = 'author';
+    authorSpan.innerText = bookData[i].Author;
+
+    const byText = document.createElement('span');
+    byText.innerText = 'by';
+    byText.className = 'ps-3 pe-3';
+
+    const removeBtn = document.createElement('button');
+    removeBtn.className = 'remove-btn fw-bold btn btn-outline-dark btn-sm ms-auto';
+    removeBtn.innerText = 'Remove';
+
+    if (i % 2 !== 0) {
+      li.className += ' bg-warning';
+    } else {
+      // li.className += ' bg-dark text-warning';
+      // removeBtn.className = 'remove-btn fw-bold btn btn-outline-warning btn-sm ms-auto';
+    }
+    li.append(titleSpan, byText, authorSpan, removeBtn);
+    ul.appendChild(li);
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = form.querySelector('input[placeholder="Title"]').value;
+  const author = form.querySelector('input[placeholder="Author"]').value;
+
+  const newData = {
+    Title: title,
+    Author: author,
+  };
+
+  bookData.push(newData);
+  saveDataToLocalStorage();
+  renderBookList();
+  form.reset();
+});
+
+ul.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const li = e.target.parentElement;
+    const index = Array.prototype.indexOf.call(ul.children, li);
+
+    bookData.splice(index, 1);
+    saveDataToLocalStorage();
+    renderBookList();
+  }
+});
+
+for (let i = 0; i < ul.children; i += 2) {
+  // const ul = document.getElementById('book-list-container');
+  // const list = Array.prototype.slice.call(ul.children);
+  // console.log(ul.children[1]);
+  ul.children[1].className += ' bg-warning';
+}
+renderBookList();
