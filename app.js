695930695fb5e11898aa7336dc709a2964a874db
koppai <psthu.koppai@gmail.com>
2023-05-30 20:30:58 +0630

    Add css styling

--- app.js
+++ app.js
@@ -8,25 +8,36 @@ function saveDataToLocalStorage() {
 
 function renderBookList() {
   ul.innerHTML = '';
-  bookData.forEach((data) => {
+  // bookData.forEach((data) => {
+  for (let i = 0; i < bookData.length; i += 1) {
     const li = document.createElement('li');
+    li.className = 'px-3 py-2 d-flex fw-bold fs-5';
+
     const titleSpan = document.createElement('span');
     titleSpan.className = 'title';
-    titleSpan.innerText = data.Title;
+    titleSpan.innerText = `"${bookData[i].Title}"`;
 
     const authorSpan = document.createElement('span');
     authorSpan.className = 'author';
-    authorSpan.innerText = data.Author;
+    authorSpan.innerText = bookData[i].Author;
+
+    const byText = document.createElement('span');
+    byText.innerText = 'by';
+    byText.className = 'ps-3 pe-3';
 
     const removeBtn = document.createElement('button');
-    removeBtn.className = 'remove-btn';
+    removeBtn.className = 'remove-btn fw-bold btn btn-outline-dark btn-sm ms-auto';
     removeBtn.innerText = 'Remove';
 
-    li.appendChild(titleSpan);
-    li.appendChild(authorSpan);
-    li.appendChild(removeBtn);
+    if (i % 2 !== 0) {
+      li.className += ' bg-warning';
+    } else {
+      // li.className += ' bg-dark text-warning';
+      // removeBtn.className = 'remove-btn fw-bold btn btn-outline-warning btn-sm ms-auto';
+    }
+    li.append(titleSpan, byText, authorSpan, removeBtn);
     ul.appendChild(li);
-  });
+  }
 }
 
 form.addEventListener('submit', (e) => {
@@ -57,4 +68,10 @@ ul.addEventListener('click', (e) => {
   }
 });
 
+for (let i = 0; i < ul.children; i += 2) {
+  // const ul = document.getElementById('book-list-container');
+  // const list = Array.prototype.slice.call(ul.children);
+  // console.log(ul.children[1]);
+  ul.children[1].className += ' bg-warning';
+}
 renderBookList();
